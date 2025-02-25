24ddb57b18657b33e385be846c5c049470b85e64
Jakub Musialowski <mds@mds.biz.pl>
2025-02-25 23:11:38 +0100

    Add menu and search wrapper

--- src/common/NavBar/index.js
+++ src/common/NavBar/index.js
@@ -1,9 +1,14 @@
-import { StyledNavBar } from "./styled";
+import { StyledMenu, StyledMenuItem, StyledNavBar, StyledSearch } from "./styled";
 import logo from "../assets/logo.png"
 
 const NavBar = () => (
     <StyledNavBar>
         <img src={logo} alt="logo" />
+        <StyledMenu>
+            <StyledMenuItem>Movies</StyledMenuItem>
+            <StyledMenuItem>People</StyledMenuItem>
+        </StyledMenu>
+        <StyledSearch />
     </StyledNavBar>
 );
 
--- src/common/NavBar/styled.js
+++ src/common/NavBar/styled.js
@@ -7,4 +7,37 @@ export const StyledNavBar = styled.div`
     display: flex;
     justify-content: flex-start;
     align-items: center;
+    gap: 80px;
+`
+
+export const StyledMenu = styled.ul`
+    display: flex;
+    gap: 16px;
+    list-style: none;
+`
+
+export const StyledMenuItem = styled.li`
+    color: ${({ theme }) => theme.colors.white};
+    text-transform: uppercase;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 1;
+    padding: 14px 24px;
+    border: 1px solid transparent;
+    border-radius: 24px;
+    cursor: pointer;
+    transition: all 0.35s ease;
+
+    &:hover {
+        border: 1px solid ${({ theme }) => theme.colors.white};
+    }
+`
+
+export const StyledSearch = styled.div`
+    height: 48px;
+    width: 432px;
+    margin-inline-start: auto;
+    background: ${({ theme }) => theme.colors.white};
+    border: 1px solid ${({ theme }) => theme.colors.grey};
+    border-radius: 33px;
 `
