10304e3b2e4e00b79bdc45010a0cfa55e9836147
Jakub Musialowski <mds@mds.biz.pl>
2025-03-02 00:06:22 +0100

    Add imgf and styling to no result page

--- src/common/NoResults/index.js
+++ src/common/NoResults/index.js
@@ -1,10 +1,13 @@
 import { PageTitle } from "../PageHeader"
 import { PageContainer } from "../styled/page-container";
+import noresult from "../assets/no-results.png"
+import { StyledImg } from "./styled";
 
 const NoResults = () => {
     return (
         <PageContainer>
             <PageTitle title="Sorry, there are no results for “Muan”" />
+            <StyledImg src={noresult} alt="No results" />
         </PageContainer>
     )
 };
