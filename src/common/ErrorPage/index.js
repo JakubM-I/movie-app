8d045b10c838ece9796f8f0d54e57f92b38d167a
Jakub Musialowski <mds@mds.biz.pl>
2025-03-02 12:24:47 +0100

    Styling error page

--- src/common/ErrorPage/index.js
+++ src/common/ErrorPage/index.js
@@ -1,15 +1,23 @@
-import { PageTitle } from "../PageHeader";
 import { PageContainer } from "../styled/page-container";
 import warning from "../assets/warning.png"
+import { ErrorPageTitle, StyledButton, StyledErrorPage, StyledParagraph, StyledWarningImg } from "./styled";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const ErrorPage = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/");
+    }
+
     return (
         <PageContainer>
-            <div>
-                <img src={warning} alt="warning" />
-                <PageTitle title="Ooops! Something went wrong... " />
-            </div>
-
+            <StyledErrorPage>
+                <StyledWarningImg src={warning} alt="warning" />
+                <ErrorPageTitle title="Ooops! Something went wrong... " />
+                <StyledParagraph>Please check your network connection and try again</StyledParagraph>
+                <StyledButton onClick={handleClick}>Back to home page</StyledButton>
+            </StyledErrorPage>
         </PageContainer>
     )
 }
