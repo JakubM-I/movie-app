ebc6bd08dbfa446567743856d6be86f9d365dc45
Jakub Musialowski <mds@mds.biz.pl>
2025-03-05 21:33:55 +0100

    Add on input change function to search form

--- src/common/SearchForm/index.js
+++ src/common/SearchForm/index.js
@@ -1,10 +1,25 @@
+import { useSearchParams } from "react-router-dom";
 import { StyledSearch, StyledSearchIcon, StyledSearchInput } from "./styled"
 
 const SearchForm = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const query = searchParams.get("search") || "";
+
+    const onInputChange = ({ target }) => {
+        setSearchParams(target.value.trim()
+            ? { search: target.value }
+            : searchParams.delete("search"));
+    }
+
     return (
         <StyledSearch >
             <StyledSearchIcon src={searchIcon} alt="search" />
-            <StyledSearchInput type="text" placeholder="Search for movies..." />
+            <StyledSearchInput
+                type="text"
+                placeholder="Search for movies..."
+                value={query}
+                onChange={onInputChange}
+            />
         </StyledSearch>
     )
 }
