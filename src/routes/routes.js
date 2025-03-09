8eac4e1320fd6be174bd07c851b82dc312e1b8a0
irekmiernik <129183189+irekmiernik@users.noreply.github.com>
2025-03-09 20:25:54 +0100

    Changed routing

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,7 +3,7 @@ import ErrorPage from "../common/ErrorPage";
 import NoResults from "../common/NoResults";
 import ActorsListApp from "../features/actorsList/ActorsListApp";
 import MoviePageApp from "../features/moviePage/MoviePageApp";
-// import MovieListApp from "../features/movieList/MovieListApp";
+import MovieListApp from "../features/movieList/MovieListApp";
 
 
 export const routes = [
@@ -15,8 +15,7 @@ export const routes = [
             {
                 path: "/",
                 index: true,
-                // element: <MovieListApp />,
-                element: <MoviePageApp />,
+                element: <MovieListApp />,
             },
             {
                 path: "actors",
