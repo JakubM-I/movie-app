a24e3b96a7a37eb3eb9086ce0247e5e8f7326bd4
irekmiernik <129183189+irekmiernik@users.noreply.github.com>
2025-03-16 16:13:19 +0100

    Preparation configuration data for API

--- src/features/moviePage/movieAPI/dataAPI/configurationData.js
+++ src/features/moviePage/movieAPI/dataAPI/configurationData.js
@@ -3,3 +3,54 @@ export const key = "3fa6324a34b047ad2073727e56c79251";
 export const readKey = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZmE2MzI0YTM0YjA0N2FkMjA3MzcyN2U1NmM3OTI1MSIsIm5iZiI6MTc0MTk0MDQzNy42MSwic3ViIjoiNjdkM2U2ZDVkY2JhNzBhMjY5NjRlYWMxIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.brcg1d7jawabimDzPmFtmZoKykPndUNwNzFHAOJIX48";
 
 export const request = "https://api.themoviedb.org/3/movie/550?api_key=3fa6324a34b047ad2073727e56c79251";
+
+export const change_keys = ["adult", "air_date", "also_known_as", "alternative_titles", "biography", "birthday",
+  "budget", "cast", "certifications", "character_names", "created_by", "crew", "deathday", "episode", "episode_number",
+  "episode_run_time", "freebase_id", "freebase_mid", "general", "genres", "guest_stars", "homepage", "images", "imdb_id",
+  "languages", "name", "network", "origin_country", "original_name", "original_title", "overview", "parts", "place_of_birth",
+  "plot_keywords", "production_code", "production_companies", "production_countries", "releases", "revenue", "runtime", "season",
+  "season_number", "season_regular", "spoken_languages", "status", "tagline", "title", "translations", "tvdb_id", "tvrage_id",
+  "type", "video", "videos"
+];
+
+
+export const images = {
+  base_url: "http://image.tmdb.org/t/p/",
+  secure_base_url: "https://image.tmdb.org/t/p/",
+  backdrop_sizes: [
+    "w300",
+    "w780",
+    "w1280",
+    "original"
+  ],
+  logo_sizes: [
+    "w45",
+    "w92",
+    "w154",
+    "w185",
+    "w300",
+    "w500",
+    "original"
+  ],
+  poster_sizes: [
+    "w92",
+    "w154",
+    "w185",
+    "w342",
+    "w500",
+    "w780",
+    "original"
+  ],
+  profile_sizes: [
+    "w45",
+    "w185",
+    "h632",
+    "original"
+  ],
+  still_sizes: [
+    "w92",
+    "w185",
+    "w300",
+    "original"
+  ]
+};
