9636e3513f18f54e1f5409ad6972ed4a9d5bd7ff
Jakub Musialowski <mds@mds.biz.pl>
2025-03-20 22:47:06 +0100

    Add pagination to movieSaga

--- src/features/moviesList/moviesSaga.js
+++ src/features/moviesList/moviesSaga.js
@@ -1,11 +1,12 @@
-import { call, put, takeEvery, takeLatest } from "redux-saga/effects";
+import { call, delay, put, takeEvery, takeLatest } from "redux-saga/effects";
 import { getMoviesData } from "../../common/utils/getMoviesData";
-import { fetchMovies, setMovies } from "./moviesSlice";
+import { currentPageSelector, fetchMovies, setMovies } from "./moviesSlice";
 
 function* fetchMoviesWorker() {
+    const page = yield select(currentPageSelector);
     console.log("fetchMoviesWorker");
     try {
-        const movies = yield call(getMoviesData);
+        const movies = yield call(getMoviesData(page));
         yield put(setMovies(movies));
     } catch (error) {
         console.error(error);
@@ -13,6 +14,33 @@ function* fetchMoviesWorker() {
     }
 }
 
+// function* paginationWorker() {
+//     try {
+//         const { page, total_pages, total_results } = yield call(getMoviesData);
+//         yield console.log(page, total_pages, total_results);
+//         yield setPagination(page, total_pages, total_results);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+function* newPageWorker() {
+    yield delay(600);
+    const page = yield select(currentPageSelector);
+    try {
+        const { movies } = yield call(getMoviesData(page));
+        yield put(setNewMoviesPage(movies));
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
 export function* moviesSaga() {
     yield takeEvery(fetchMovies.type, fetchMoviesWorker);
+    yield takeEvery(fetchMovies.type, paginationWorker);
+    yield takeEvery(setNextPage.type, newPageWorker);
+    yield takeEvery(setPreviousPage.type, newPageWorker);
+    yield takeEvery(setLastPage.type, newPageWorker);
+    yield takeEvery(setFirstPage.type, newPageWorker);
 }
