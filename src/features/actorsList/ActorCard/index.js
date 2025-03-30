8ea0a4ecd14342d42a28b6b5d4738c1bdd7020d4
Jakub Kulik <jakub.kulik1@o2.pl>
2025-03-30 20:45:15 +0200

    correct form of actor card

--- src/features/actorsList/ActorCard/index.js
+++ src/features/actorsList/ActorCard/index.js
@@ -11,8 +11,7 @@ export const ActorCard = () => {
 
   const moviesActor = useSelector(moviesActorSelector);
   console.log(moviesActor);
-
-
+  // const getMovieGenres = (moviesActor, moviesActorId) => {
   return (
     <ActorCardContainer>
       <ActorImage src="https://image.tmdb.org/t/p/w500/aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg" />
