3892bd6f158e7cd6ddbd13b2952e1b0537d38171
irekmiernik <129183189+irekmiernik@users.noreply.github.com>
2025-05-12 19:58:56 +0200

    Refactoring styling fo rmoviePage

--- src/features/moviePage/People/styled.js
+++ src/features/moviePage/People/styled.js
@@ -1,11 +1,27 @@
 import styled from "styled-components";
 
 export const PeopleWrapper = styled.div`
-  margin: 64px auto 0px;
-  width: 100%;
+  max-width: 100vw;
+  margin: 40px auto 0px;
+
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    max-width: 288px;
+    margin: 6px auto 0px;
+  }
 `;
 
 export const SubHeader = styled.h2`
-  margin: 0px 0px 0px 2px; 
-  width: 100%;
+  max-width: 100%;
+  margin: 0px 0px 32px 0px;
+  padding-left: 16px;
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 1.2;
+  
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    max-width: 100%;
+    margin: 0px 0px 12px 0px;
+    padding-left: 8px;
+    font-size: 20px;
+  }  
 `;
