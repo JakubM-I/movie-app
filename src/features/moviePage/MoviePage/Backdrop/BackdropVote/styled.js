35359a48d410bdea759950e32ff282be8a48d004
Jakub Musiałowski <mds@mds.biz.pl>
2025-07-19 19:56:13 +0200

    Movie page styling fixed

--- src/features/moviePage/MoviePage/Backdrop/BackdropVote/styled.js
+++ src/features/moviePage/MoviePage/Backdrop/BackdropVote/styled.js
@@ -7,7 +7,9 @@ export const Wrapper = styled.div`
   flex-wrap: wrap;
   align-items: baseline;
   justify-content: flex-start;
-  font-size: 14px;
+  gap: 8px;
+  row-gap: 16px;
+  font-size: 16px;
   color: ${({ theme }) => theme.colors.white};
 
   @media(max-width: ${({ theme }) => theme.breakpoints.laptopMax}px) {
@@ -20,6 +22,8 @@ export const Wrapper = styled.div`
     margin-top: 12px;  
   }
   @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    flex-wrap: nowrap;
+    gap: 4px;
     margin-top: 8px;  
   }
   @media(max-width: ${({ theme }) => theme.breakpoints.mobileVerticalMax}px) {
@@ -29,34 +33,39 @@ export const Wrapper = styled.div`
 
 export const CounterWrapper = styled.div`
   display: flex;
-  flex-wrap: wrap;
+  flex-wrap: nowrap;
   align-items: baseline;
   justify-content: flex-start;
-  font-size: 14px;
+  gap: 7px;
+  margin-inline-end: 4px;
+  font-size: 16px;
+  font-weight: 400;
 
   @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     margin: 0px;
     font-size: 13px;
+    gap: 2px;
+    margin-inline-end: 4px;
   }
 `
 
 export const Star = styled(StarIcon)`
-  height: 24px;
-  width: 24px;
-  margin-inline-end: 8px;
+  height: 40px;
+  width: 40px;
+  /* margin-inline-end: 8px; */
   align-self: flex-start;
 
   @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     height: 16px;
     width: 16px;
-    padding-block-end: 2px;
+    /* padding-block-end: 2px; */
   }
 `;
 
 export const Average = styled.strong`
-  font-size: 22px;
+  font-size: 30px;
   font-weight: 500;
-  margin-inline-end: 8px;
+  /* margin-inline-end: 8px; */
   line-height: 1.3;
 
   @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
@@ -66,18 +75,21 @@ export const Average = styled.strong`
 `;
 
 export const OutOf = styled.span`
-  margin-inline-end: 8px;
+  /* margin-inline-end: 8px; */
   line-height: 1.2;
+  /* margin-inline-end: 4px; */
 `;
 
 export const Count = styled.p`
   margin: 0px;
   line-height: 1.2;
+  width: 100%;
 
   @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     font-weight: 400;
     font-size: 13px;
     line-height: 1.3;
+    width: auto;
     color: ${({ theme }) => theme.colors.darkerGrey};
   }
 `;
